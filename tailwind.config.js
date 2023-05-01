a8e3553d99921650a1a3b6e3f7df24b04f0a01d6
Egor <egorviliavin@gmail.com>
2023-05-01 16:34:27 +0300

    Load

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.html'],
+  theme: {
+    colors: {
+      bg: '#1E1E1E',
+      primary: '#C778DD',
+      grey: '#ABB2BF',
+      white: '#FFFFFF'
+        },
+        fontFamily:{
+          'mono': 'Fira Code'
+        },
+    extend: { 
+    }
+  },
+  plugins: [],
+}
+
